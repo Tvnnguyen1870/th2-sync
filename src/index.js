fdd0af19cae30b0a51a22bf8757329a452d641c5
nguyen <tovannguyen12354@gmail.com>
2023-09-24 10:35:25 +0700

    th2 sync

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const user = {
+  name: 'hungnv0902'
+}
+  
+async function getUserProfile() {
+  try {
+    const githubUser = await axios.get(`https://api.github.com/users/${user.name}`);
+    console.log(githubUser.data)
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+getUserProfile();
+
